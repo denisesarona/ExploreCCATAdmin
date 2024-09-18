ab3a883cd8395e5924f3d9c1fde5b6043893d6e2
denisesarona <135094209+denisesarona@users.noreply.github.com>
2024-09-18 17:38:06 +0800

    Fix forgot password layout

--- forgotPassword.php
+++ forgotPassword.php
@@ -1,25 +1,25 @@
-<!--------------- INCLUDES --------------->
+<!--------------- INCLUDES ---------------> 
 <?php 
     include('includes/header.php');
 ?>
-<!--------------- CSS --------------->
-<link rel="stylesheet" href="assets/css/forgetPassword.css">    
+<!--------------- CSS ---------------> 
+<link rel="stylesheet" href="assets/css/login.css">    
 
-
-<a href="login.php" class="back-btn">
+<div class="back-btn-container">
+    <a href="index.php" class="back-btn">
         <i class='bx bx-arrow-back'></i>
     </a>
-
-    <div class="wrapper">
-        <form action="functions/authcode.php" method="POST">
-            <h1>Forgot your password</h1>
-            <div class="word">
-                <p>Enter your Email Address</p>
-            </div>
-            <div class="input-box">
-                <input type="text" class="form-control" placeholder="Enter email Address" name="email" required>
-            </div>
-        
-            <button type="submit" name="forgotPass" class="btn">CONTINUE</button> 
-        </form>
 </div>
+
+<div class="wrapper">
+    <form action="functions/authcode.php" method="POST">
+        <h1  style="font-size: 28px">Forgot your password?</h1>
+        <div class="word text-center">
+            <p>Enter your Email Address</p>
+        </div>
+        <div class="input-box">
+            <input type="text" class="form-control" placeholder="Enter email Address" name="email" required>
+        </div>
+        <button type="submit" name="forgotPass" class="btn">CONTINUE</button> 
+    </form>
+</div> 
