87f16a2495f2dc7d3fcfe76e89c07eb433f22914
denisesarona <135094209+denisesarona@users.noreply.github.com>
2024-09-18 12:34:16 +0800

    Created getbyid function

--- admin/facultyDetails.php
+++ admin/facultyDetails.php
@@ -1,46 +1,79 @@
 <?php 
-    session_start();
     include('includes/header.php');
+    include('../functions/queries.php');
 ?>
-<link rel="stylesheet" href="assets/css/facultyDetails.css"> 
+<!--------------- VIEW AND EDIT FACULTY MEMBERS DETAILS PAGE --------------->
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <?php
+                if(isset($_GET['id'])){
+                    $id = $_GET['id'];
+                    $category = getByID('categories', $id);
 
-<section class="dashboard">
-    <div class="register-container">
-        <div class="title">
-            <span class="text">FACULTY MEMBER'S DETAILS</span>
+                    if(mysqli_num_rows($category) > 0){
+                        $data = mysqli_fetch_array($category);
+            ?>  
+                        <div class="card mt-4">
+                        <div class="card-header">
+                            <h4 style="font-family: 'Poppins', sans-serif; font-size: 35px;">Edit Category</h4>
+                        </div>
+                            <div class="card-body">
+                                <!--------------- FORM--------------->
+                                <form action="codes.php" method="POST" enctype="multipart/form-data">
+                                    <div class="row" style="font-family: 'Poppins', sans-serif;">
+                                        <div class="col-md-6 mb-3"> 
+                                            <div class="form-group">
+                                                <input type="hidden" name="category_id" value="<?=$data['id']; ?>">
+                                                <label for="">Name</label>
+                                                <input type="text" value="<?=$data['name']; ?>" class="form-control" placeholder="Enter Category Name" name="name" id="name">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 mb-3"> 
+                                            <div class="form-group">
+                                                <label for="">Additional Price</label>
+                                                <input type="number" value="<?=$data['additional_price']; ?>"class="form-control" placeholder="Enter Additional Price" name="additional_price">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12 mb-3"> 
+                                            <div class="form-group">
+                                                <label for="">Description</label>
+                                                <textarea class="form-control" name="description" placeholder="Enter Description" id="description" rows="3"><?=$data['description']; ?></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12 mb-3"> 
+                                            <div class="form-group">
+                                                <label for="">Upload Image</label>
+                                                <input type="file" class="form-control" name="image" id="image">
+                                                <label for="" style="margin-right: 10px;">Current Image</label>
+                                                <input type="hidden" name="old_image" value="<?=$data['image']; ?>">
+                                                <img src="../uploads/<?=$data['image']; ?>" height="50px" width="50px" alt="">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 mb-3"> 
+                                            <div class="form-group form-check">
+                                                <input type="checkbox" <?= $data['status'] ? "checked":""?> class="form-check-input CheckMe" name="status" id="status">
+                                                <label for="">Status (Check if Available) </label><br>
+                                            </div>
+                                        </div>
+                                        <!--------------- SAVE BUTTON--------------->
+                                        <div class="col-md-6 text-end">
+                                            <button type="submit" class="btn BlueBtn mt-2 md-w-10" name="editCateg_button" id="addCategSave">Update</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+            <?php
+                    }else{
+                        echo "Category not found";
+                    }
+                } else{
+                    echo "ID missing from url";
+                }
+            ?>
         </div>
-        <form class="AdminReg">
-                
-            <div class="form-row">
-                <div class="form-group">
-                    <label for="department" class="form-label">Department</label>
-                    <select id="department" name="department" class="form-control" placeholder="Choose depeartment">
-                        <option value="" >DCS</option>
-                        <option value="" >DMS</option>
-                        <option value="" >DTE</option>
-                        <option value="" >DIT</option>
-                        <option value="" >DE</option>
-                    </select>
-                </div>
-            </div>
-        
-            <div class="form-row">
-                <div class="form-group">
-                    <label for="firstn" class="form-label">Name</label>
-                    <input type="text" name="name" class="form-control" id="name" placeholder="Enter faculty name">
-                </div>
-                <div class="form-group">
-                    <label for="em" class="form-label">Position</label>
-                    <input type="text" name="Position" class="form-control" id="Position" placeholder="Enter faculty position">
-                </div>
-            </div>
-
-            <div class="form-group">
-                <label for="image">Upload Image</label>
-                <input type="file" class="form-control" id="image" name="image" accept="image/*">
-            </div>
-        
-            <button type="submit" class="btn">SAVE</button>
-        </form>
     </div>
-</section>
+</div>
+<!--------------- FOOTER --------------->
+<?php include('includes/footer.php');?>
--- functions/queries.php
+++ functions/queries.php
@@ -6,4 +6,10 @@
         $query = "SELECT * FROM $table";
         return $query_run = mysqli_query($con,$query);
     }
+
+    function getByID($table, $id){ // FETCH DATA FROM SPECIFIC TABLE BASED ON ID
+        global $con; // ACCESS GLOBAL DATABASE VARIABLE
+        $query = "SELECT * FROM $table WHERE id='$id'"; // SQL QUERY O SELECT ALL DATA FROM THE TABLE WHERE ID MATCHES THE PROVIDED ID
+        return $query_run = mysqli_query($con, $query); // EXECUTE QUERY AND RETURN RESULT
+    }
 ?>
