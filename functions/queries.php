10e3ca20955932fa283a27cc162ce567eb733b20
denisesarona <135094209+denisesarona@users.noreply.github.com>
2024-09-23 20:40:27 +0800

    Filter out nodes by department

--- functions/queries.php
+++ functions/queries.php
@@ -50,7 +50,7 @@
         // Prepare the SQL statement
         $sql = "SELECT faculty_id AS id, name, position AS position, img AS img, department, pid 
                 FROM facultytb 
-                WHERE department = ?"; // Assuming 'department' is the ID
+                WHERE dept_id = ?"; // Assuming 'department' is the ID
     
         // Prepare the statement
         $stmt = $con->prepare($sql);
@@ -86,7 +86,6 @@
         
         return $nodes; // Return the array of nodes
     }
-    
     
     
     function getDepartments($con) {
